7860e835494fe25bdf6d5238d19b4ea4bfe26769
Cyber-Buddy <122369607+Cyber-Buddy@users.noreply.github.com>
2023-01-12 10:25:19 +0530

    Update apkhunt.go

--- apkhunt.go
+++ apkhunt.go
@@ -45,7 +45,7 @@ import (
         log.Println("[+] APKHunt by RedHunt Labs - A Modern Attack Surface (ASM) Management Company")
         log.Println("[+] Based on: OWASP MASVS - https://mobile-security.gitbook.io/masvs/")
         log.Println("[+] Author: Sumit Kalaria & Mrunal Chawda (RHL PenTest Team)")
-        log.Println("[*] Connect: Please do write to us at apkhunt@redhuntlabs.com for any suggestions/feedback.")
+        log.Println("[*] Connect: Please do write to us for any suggestions/feedback.")
         log.Println("[*] Remember: Continuously track your Attack Surface using https://redhuntlabs.com/nvadr.")
         }
 
