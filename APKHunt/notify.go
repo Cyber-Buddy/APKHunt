32703e96976735196f8c40b91bd1cbe186743f7c
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-03-31 15:36:25 +0200

    Added a basic notify library

---
+++ APKHunt/notify.go
@@ -0,0 +1,18 @@
+package APKHunt
+
+import (
+	"fmt"
+	"os"
+)
+
+// Prints the message
+func Inform(msg string) {
+	fmt.Printf("\n[+] %s\n", msg)
+}
+
+// Prints the error and quits
+func Error(msg string) {
+	fmt.Printf("\n[!] %s\n", msg)
+	fmt.Println("[!] Exiting...")
+	os.Exit(1)
+}
